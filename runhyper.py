3a28b919daf80c4fbc670221b1a3597a1f485b40
myouness <mansaryouness@gmail.com>
2016-12-03 18:53:16 +0100

    hyperopt sklearn

--- runhyper.py
+++ runhyper.py
@@ -27,26 +27,26 @@ max_evals = 50
 #optimizehyper(space, model, modelname, max_evals, data)
 
 #
-model = HuberRegressor()
-space = {
-'alpha' : hp.loguniform('alpha', -10, 0),
-'epsilon' : hp.uniform('epsilon', 1.01, 3000)
-         }
-modelname = "HuberRegressor1"
-optimizehyper(space, model, modelname, max_evals, data)
-
+#model = HuberRegressor()
+#space = {
+#'alpha' : hp.loguniform('alpha', -10, 0),
+#'epsilon' : hp.uniform('epsilon', 1.01, 3000)
+#         }
+#modelname = "HuberRegressor1"
+#optimizehyper(space, model, modelname, max_evals, data)
 #
-model = PassiveAggressiveRegressor()
-space = {
-'C' : hp.loguniform('C', -10, 2),
-         }
-modelname = "PassiveAggressiveRegressor1"
-optimizehyper(space, model, modelname, max_evals, data)
+##
+#model = PassiveAggressiveRegressor()
+#space = {
+#'C' : hp.loguniform('C', -10, 2),
+#         }
+#modelname = "PassiveAggressiveRegressor1"
+#optimizehyper(space, model, modelname, max_evals, data)
 #
 model = SGDRegressor()
 space = {
 'alpha' : hp.loguniform('alpha', -10, 0),
-'l1_ratio' : hp.loguniform('l1_ratio', -1, 1)
+'l1_ratio' : hp.loguniform('l1_ratio', -10, 0)
          }
 modelname = "SGDRegressor1"
 optimizehyper(space, model, modelname, max_evals, data)
